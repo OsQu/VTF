7d11a3c589c1a60f02e46048be58783527627c7d
Oskari Virtanen <oskari@flowdock.com>
2013-04-04 23:21:50 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VTF::Application.config.secret_token = '3c65d3af190df8c263ca46f6dc9aaef7b0c1f09df406cc7c72df497abb50a7e4f4f5ae11a7537af2f61123b9c9e461cfca31889925bf29b189c4ec00255c0ddb'
