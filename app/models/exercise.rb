ce79a3619246b1b8015d72727afb2e8f87a57aa3
Oskari Virtanen <oskari@flowdock.com>
2013-04-09 15:46:07 +0300

    Exercise validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,8 @@
 class Exercise < ActiveRecord::Base
+  CATEGORIES = %w(SQL-Injection XSS CSRF Missing\ HTTPS)
+
   attr_accessible :description, :name, :category
+
+  validates :name, :presence => true
+  validates :category, :inclusion => { :in => CATEGORIES, :message => "%{value} is not correct category" }
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Exercise do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it "should allow correct cateogry" do
+      e = Exercise.new category: "XSS", name: "Ex 1.", description: "Blabla"
+      e.save!
+
+      Exercise.find_by_name("Ex 1.").should_not be_nil
+    end
+
+    it "should not allow invalid cateogy" do
+      e = Exercise.new category: "Invalid", name: "Ex 1.", description: "Blabla"
+      expect {
+        e.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "should validate precense of name" do
+      e = Exercise.new category: "XSS", description: "Blabla"
+      expect {
+        e.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
