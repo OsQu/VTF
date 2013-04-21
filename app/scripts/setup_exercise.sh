26118d9030f61a8abf2e2ccdd352456ee81c0ab4
Oskari Virtanen <oskari.virtanen@gmail.com>
2013-04-22 01:16:57 +0300

    Debug logging and a bit optimization

--- app/scripts/setup_exercise.sh
+++ app/scripts/setup_exercise.sh
@@ -24,23 +24,31 @@ fi
 NAME=$USER$EXERCISE
 
 # Create user with correct home folder template
+echo "Creating user.."
 useradd -m -k $EXERCISE_ROOT/$EXERCISE $NAME
 
 # Create PostgreSQL user and database for user
+echo "Creating postgresql user"
 createuser -S -D -R $NAME
+echo "Creating database"
 createdb -O $NAME $NAME
 
 # Replace placeholders in with NAME
+echo "Replacing placeholder with name"
 cd /home/$NAME
 find . -type f -print0 | xargs -0 sed -i "s/PLACEHOLDER/$NAME/g"
 
 # Give correct permissions to app
+echo "Permissioins to app"
 chmod 755 public_html/app
 
 # Run setup script if present
+echo "Running the setup script if exists"
 cd public_html/app/
 
 if [ -f setup.sh ]; then
+  echo "existed! chmodding.."
   chmod u+x setup.sh
+  echo "and running"
   su $NAME -c ./setup.sh
 fi
